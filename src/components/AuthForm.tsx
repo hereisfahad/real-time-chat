5998baeeea46cef74113222e6ecf7d7f0f7b9f2f
Fahad Jabbar <fahadjabbar31@gmail.com>
2024-01-14 01:24:47 +0500

    design auth pages

---
+++ src/components/AuthForm.tsx
@@ -0,0 +1,225 @@
+'use client';
+
+import { useCallback, useEffect, useState } from 'react';
+import { signIn, useSession } from 'next-auth/react';
+import { BsGithub, BsGoogle } from 'react-icons/bs';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useRouter } from "next/navigation";
+import { toast } from "sonner"
+import axios from "axios";
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+type Variant = 'LOGIN' | 'REGISTER';
+
+const AuthForm = () => {
+  const session = useSession();
+  const router = useRouter();
+  const [variant, setVariant] = useState<Variant>('LOGIN');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const formSchema = z.object({
+    name: z.string().min(2, {
+      message: "Name must be at least 2 characters.",
+    }),
+    email: z.string().email("Enter valid email address").min(4, {
+      message: "Email must be at least 4 characters.",
+    }),
+    password: z.string().min(8, {
+      message: "Password must be at least 8 characters.",
+    }),
+  })
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: ''
+    },
+  })
+
+  useEffect(() => {
+    if (session?.status === 'authenticated') {
+      router.push('/rooms')
+    }
+  }, [session?.status, router]);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === 'LOGIN') {
+      setVariant('REGISTER');
+    } else {
+      setVariant('LOGIN');
+    }
+  }, [variant]);
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    setIsLoading(true);
+
+    if (variant === 'REGISTER') {
+      axios.post('/api/register', data)
+        .then(() => signIn('credentials', {
+          ...data,
+          redirect: false,
+        }))
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Invalid credentials!")
+          }
+
+          if (callback?.ok) {
+            router.push('/rooms')
+          }
+        })
+        .catch(() => {
+          toast.error('Something went wrong!')
+        }
+        )
+        .finally(() => setIsLoading(false))
+    }
+
+    if (variant === 'LOGIN') {
+      signIn('credentials', {
+        ...data,
+        redirect: false
+      })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error('Invalid credentials!');
+          }
+
+          if (callback?.ok) {
+            router.push('/rooms')
+          }
+        })
+        .finally(() => setIsLoading(false))
+    }
+  }
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    signIn(action, { redirect: false })
+      .then((callback) => {
+        if (callback?.error) {
+          toast.error('Invalid credentials!');
+        }
+
+        if (callback?.ok) {
+          router.push('/rooms')
+        }
+      })
+      .finally(() => setIsLoading(false));
+  }
+
+  return (
+    <div className="sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            {variant === 'REGISTER' && (
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Fahad" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email address</FormLabel>
+                  <FormControl>
+                    <Input placeholder="fahad@email.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div>
+              <Button disabled={isLoading} type="submit" className='w-full'>
+                {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+              </Button>
+            </div>
+          </form>
+        </Form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2 justify-center">
+            <Button
+              size="icon"
+              variant="outline"
+              onClick={() => socialAction('github')}
+            >
+              <BsGithub className="h-4 w-4" />
+            </Button>
+            <Button
+              size="icon"
+              variant="outline"
+              onClick={() => socialAction('google')}
+            >
+              <BsGoogle className="h-4 w-4" />
+            </Button>
+          </div>
+        </div>
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>
+            {variant === 'LOGIN' ? 'New user?' : 'Already have an account?'}
+          </div>
+          <div
+            onClick={toggleVariant}
+            className="underline cursor-pointer"
+          >
+            {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AuthForm;

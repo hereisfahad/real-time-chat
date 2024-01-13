fc8a9c009f3fa803be533954d72e2449f1c201ed
Fahad Jabbar <mrfahadjabbar@gmail.com>
2024-01-14 03:29:07 +0500

    Login & Register functionality (#1)
    * design auth pages
    
    * feat: implement login & register workflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Toaster } from "@/components/ui/sonner"
+
 import './globals.css'
+import AuthContext from '@/context/AuthContext';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthContext>
+          <main>{children}</main>
+        </AuthContext>
+        <Toaster />
+      </body>
     </html>
   )
 }

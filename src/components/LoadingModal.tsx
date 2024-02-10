232b047198262151a766d1140aeba97fc308f907
Fahad Jabbar <fahadjabbar31@gmail.com>
2024-02-10 15:19:06 +0500

    make loading modal spinner smaller

--- src/components/LoadingModal.tsx
+++ src/components/LoadingModal.tsx
@@ -3,7 +3,7 @@ import { ArrowPathIcon } from "@heroicons/react/24/solid";
 const LoadingModal = () => {
   return (
     <div className="fixed z-[100] inset-0 w-full h-full bg-gray-500 bg-opacity-75 flex items-center justify-center">
-      <ArrowPathIcon className="animate-spin rounded-full h-8 w-8" />
+      <ArrowPathIcon className="animate-spin rounded-full h-6 w-6" />
     </div>
   )
 }
